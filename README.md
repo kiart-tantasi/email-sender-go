8c78419920f55dca100dbd1558d2eaa096b0e070
kiart-tantasi <kiarttantasi@gmail.com>
2025-01-16 23:25:05 +0700

    add getEnv fn

--- README.md
+++ README.md
@@ -8,4 +8,7 @@ docker compose up
 Run go app to send email
 ```
 go run ./apps/emailsender/cmd/emailsender/main.go
+
+# with env vars
+SMTP_HOST=? SMTP_PORT=? SMTP_USERNAME=? SMTP_PASSWORD=? go run apps/emailsender/cmd/emailsender/main.go
 ```
--- apps/emailsender/cmd/emailsender/main.go
+++ apps/emailsender/cmd/emailsender/main.go
@@ -4,26 +4,48 @@ import (
 	"fmt"
 	"net/smtp"
 	"os"
+	"time"
 )
 
 func main() {
-	smtpHost := "localhost"
-	smtpPort := "25"
+	smtpHost := getEnv("SMTP_HOST", "localhost")
+	smtpPort := getEnv("SMTP_PORT", "25")
+	smtpUsername := getEnv("SMTP_USERNAME", "username")
+	smtpPassword := getEnv("SMTP_PASSWORD", "password")
+	auth := smtp.PlainAuth("identity", smtpUsername, smtpPassword, smtpHost)
 
-	from := "testfrom@example.com"
-	to := []string{"testto@example.com"}
-	subject := "Test subject"
-	body := "Test body."
-	message := []byte(
-		fmt.Sprintf("Subject: %s\r\n\r\n%s", subject, body),
-	)
+	fmt.Println("running with SMTP host", smtpHost)
+	errCount := 0
+	start := time.Now()
+	for i := 0; i < 100; i++ {
+		// send email
+		from := "kiarttantasi@gmail.com"
+		to := []string{"kiarttantasi@gmail.com"}
+		subject := "Test subject"
+		body := fmt.Sprintf("Test body: %d", i)
+		message := []byte(
+			fmt.Sprintf("Subject: %s\r\n\r\n%s", subject, body),
+		)
+		err := smtp.SendMail(smtpHost+":"+smtpPort, auth, from, to, message)
+		if err != nil {
+			errCount++
+			fmt.Println(err)
+		}
 
-	auth := smtp.PlainAuth("identity", "username", "password", smtpHost)
-	err := smtp.SendMail(smtpHost+":"+smtpPort, auth, from, to, message)
-	if err != nil {
-		fmt.Println("Failed to send email:", err)
-		os.Exit(1)
+		// progress
+		if i%10 == 0 {
+			fmt.Println("Sent email index", i)
+		}
 	}
 
-	fmt.Println("Email sent successfully!")
+	fmt.Println("done in", time.Since(start).Milliseconds())
+	fmt.Println("errCount:", errCount)
+}
+
+func getEnv(envName, defaultValue string) string {
+	val := os.Getenv(envName)
+	if val != "" {
+		return val
+	}
+	return defaultValue
 }
