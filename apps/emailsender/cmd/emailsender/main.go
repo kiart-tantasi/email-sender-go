7e13367421ff2350e2e5ad56fdd1529c678febe3
kiart-tantasi <kiarttantasi@gmail.com>
2025-01-24 19:02:20 +0700

    change fake avc html-gen time

--- apps/emailsender/cmd/emailsender/main.go
+++ apps/emailsender/cmd/emailsender/main.go
@@ -13,21 +13,23 @@ import (
 // 10,000 emails, 5 goroutines, 38232 ms
 // 10,000 emails, 10 goroutines, 37747 ms
 // 10,000 emails, 20 goroutines, 37044 ms
-// 100,000 emails, 5 goroutines, 379796 ms
-// 100,000 emails, 10 goroutines, 365820 ms
 
 // record macbook air m2, with html generation
-// 10,000 emails, 5 goroutines, 120296 ms
+// 10,000 emails, 1 goroutines, 245451 ms
+// 10,000 emails, 5 goroutines, 50875 ms
+// 10,000 emails, 10 goroutines, 35820 ms
+// 10,000 emails, 20 goroutines, 34750 ms
 
 func main() {
+	// env
 	smtpHost := getEnv("SMTP_HOST", "localhost")
 	smtpPort := getEnv("SMTP_PORT", "25")
 	smtpUsername := getEnv("SMTP_USERNAME", "username")
 	smtpPassword := getEnv("SMTP_PASSWORD", "password")
 	auth := smtp.PlainAuth("identity", smtpUsername, smtpPassword, smtpHost)
 
-	// goroutine config
-	goroutineLimit := 5
+	// goroutine amount
+	goroutineLimit := 20
 	limitChannel := make(chan int, goroutineLimit)
 	var wg sync.WaitGroup
 
@@ -36,7 +38,7 @@ func main() {
 	if smtpHost != "localhost" {
 		emailAmount = 1
 	}
-	fmt.Println("running with SMTP host", smtpHost, "with", emailAmount, "email(s)")
+	fmt.Println("running with SMTP host", smtpHost, "with", emailAmount, "email(s), with", goroutineLimit, "concurrent goroutine(s)")
 
 	successCount := 0
 	errCount := 0
@@ -87,6 +89,6 @@ func getEnv(envName, defaultValue string) string {
 }
 
 func fakeGenerateHtmlTime() {
-	// avg time when communicating through kube service dns
-	time.Sleep(50 * time.Millisecond)
+	// avg time when communicating through kube dns
+	time.Sleep(15 * time.Millisecond)
 }
